6eaa53ece90af98705b77d8d78d597df5165b3fe
Nils Rietze <nils.rietze@uzh.ch>
2023-04-25 15:55:43 +0200

    Changed paths in resampling script.

--- code/classification/0_resampling.py
+++ code/classification/0_resampling.py
@@ -2,102 +2,70 @@ import os
 from glob import glob
 import subprocess
 
+fpath = "../../../data/mosaics/"
+os.chdir(fpath)
+
 for year in [2020,2021]:
     
     for site in ['TLB','Ridge','CBH']:
         
-        for sens in ['TIR','MSP','DSM']:
+        for sens in ['thermal','msp']:
             
             # 1. CNOFIGURE PATHS AND LOAD DATA
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            fname = f"{site}_{sens}_{year}.tif"
             
-            if sens == 'TIR':
-                fn = '*_mosaic_thermal ir.tif'
-                
-                if site == 'Ridge':
-                    fn = '*_index_thermal_ir.tif'
-            
-            elif sens == 'MSP':
-                fn = 'reflectance.tif'
-                
-            elif sens == 'DSM':
-                fn = '*dsm.tif'
-            
-            PATH_IN = "C:/data/0_Kytalyk/0_drone/{}/{}/{}/original/".format(year, site,sens) 
-            
-            if not glob(PATH_IN + fn): # Check if list is empty, if yes, continue with next iteration
-                print('No data for {} from {} in {}. Skipping...'.format(sens,site,year))
+            if not glob(fname): # Check if list is empty, if yes, continue with next iteration
+                print(f'No {sens} data from {site} in {year}. Skipping...')
                 continue
             
-            PATH_IN = glob(PATH_IN + fn)[0]
-            # print(os.curdir)
-
-            fpath, fname = os.path.split(PATH_IN)
-            fname = os.path.basename(PATH_IN).split('.')[0]
-            
-            os.chdir(fpath)
-            # print(os.curdir)
-            
-            """
-            m = [re.search(pat,fpath) for pat in ['CBH','TLB','Ridge'] if re.search(pat,fpath) is not None]
-            
-            site = m[0].group(0)
-            print(site)
-            """
-            
             # Ask for the target resolution and resampling method
-            # t_res = input('Type in your target resoultion (in m): \n')
             t_res = str(.15)
             t_res_neg = str(-.15)
             
+            # list of possible resampling methods, we used average
             methodlist = ('\n').join(['near','bilinear','cubic','cubicspline','lanczos','average'])
-            # print(methodlist)
-            # method = input('Provide the interpolation method: \n')
             method = 'average'
             
-            # Ask for output directory
-            # PATH_OUT = Tk()
-            # PATH_OUT.withdraw()
-            # PATH_OUT = filedialog.askdirectory(initialdir = "D:/2_data/1_drone",
-            #                                    title = "Where should I store the resampled TIFFs?")
-            PATH_OUT = "C:/data/0_Kytalyk/0_drone/{}/{}/{}/resampled/".format(year, site,sens)
-            
             # Concatenate list to form output filename
-            fname_out = '_'.join([fname, method,t_res,'resampled.tif'])
+            fname_out = f"{site}_{sens}_{year}_resampled.tif"
             
-            if os.path.exists(PATH_OUT+fname_out):
+            if os.path.exists(fname_out):
                 print('File already exists. Skipping... \n')
                 continue
             
-            # Define target extent (necessary for grid alignment):  <xmin ymin xmax ymax>
+            # Define target extent (necessary for grid alignment):  <x_min y_min x_max y_max>
             if site == 'TLB':
                 # TLB 2021 proj4
                 proj_TIR = '+proj=utm +zone=55 +datum=WGS84 +units=m +no_defs +x_0=517910.084200000041164 +y_0=7859161.502500000409782'
-                # proj_MSP = '+proj=utm +zone=55 +datum=WGS84 +units=m +no_defs +x_0=517856.149270000052638 +y_0=7859211.258260000497103'
-                xmin = 517910.084
-                xmax = 518582.234
-                ymin = 7858641.003
-                ymax = 7859161.503
+                x_min = 517910.084
+                x_max = 518582.234
+                y_min = 7858641.003
+                y_max = 7859161.503
                 
             elif site == 'Ridge':
                 # Ridge 2021 proj4
                 proj_TIR = '+proj=utm +zone=55 +datum=WGS84 +units=m +no_defs +x_0=516468.945960000040941 +y_0=7858648.509120000526309'
-                # proj_MSP = '+proj=utm +zone=55 +datum=WGS84 +units=m +no_defs +x_0=517856.149270000052638 +y_0=7859211.258260000497103'
-                xmin = 516468.945957
-                xmax = 517216.743612
-                ymin = 7858142.321060
-                ymax = 7858648.509123
+                x_min = 516468.945957
+                x_max = 517216.743612
+                y_min = 7858142.321060
+                y_max = 7858648.509123
                 
             elif site == 'CBH':
                 # CBH 2021 proj4
                 proj_TIR = '+proj=utm +zone=55 +datum=WGS84 +units=m +no_defs +x_0=517081.748800000059418 +y_0=7859026.249200000427663'
-                xmin = 517081.7488
-                xmax = 517794.0988
-                ymin = 7858449.7992
-                ymax = 7859026.2492
+                x_min = 517081.7488
+                x_max = 517794.0988
+                y_min = 7858449.7992
+                y_max = 7859026.2492
             
             # Run GDALWARP as cmd in subprocess
-            print('Resampling {} from {} in {}'.format(sens,site,year))
-            args = ['gdalwarp', '-t_srs', proj_TIR, '-r', method, '-te', str(xmin), str(ymin), str(xmax), str(ymax),'-tr', t_res, t_res_neg,   PATH_IN, PATH_OUT + '/' + fname_out]
+            print(f'Resampling {sens} from {site} in {year}')
+            args = ['gdalwarp', 
+                    '-t_srs', proj_TIR, 
+                    '-r', method, 
+                    '-te', str(x_min), str(y_min), str(x_max), str(y_max),
+                    '-tr', t_res, t_res_neg,
+                    fname, fname_out]
             subprocess.Popen(args)
         
